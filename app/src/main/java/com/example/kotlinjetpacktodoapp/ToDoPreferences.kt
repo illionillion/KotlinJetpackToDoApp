d499aef4bac5552ad4e000dbaf1a09de8ae2785f
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-01 10:56:35 +0900

    SharedPreference編集 todoのカウントも保存できるようにした

--- app/src/main/java/com/example/kotlinjetpacktodoapp/ToDoPreferences.kt
+++ app/src/main/java/com/example/kotlinjetpacktodoapp/ToDoPreferences.kt
@@ -2,19 +2,26 @@ package com.example.kotlinjetpacktodoapp
 import android.content.Context
 
 public class ToDoPreferences(context: Context) {
-    private val sharedPreferences = context.getSharedPreferences("com.example.kotlinsimpletodoapp.TaskPreferences", Context.MODE_PRIVATE)
+    private val sharedPreferences = context.getSharedPreferences("com.example.kotlinJetpackTodoApp.ToDoPreferences", Context.MODE_PRIVATE)
+
+    fun saveToDoCount(count: Int) {
+        sharedPreferences.edit().putInt("toDoCount", count).apply()
+    }
+    fun loadToDoCount(): Int {
+        return sharedPreferences.getInt("toDoCount", 0)
+    }
 
     fun saveToDoList(taskList: MutableList<TaskItem>) {
         val json = StringBuilder()
         for (taskItem in taskList) {
             json.append("${taskItem.id},${taskItem.taskName},${taskItem.isCompleted}|")
         }
-        sharedPreferences.edit().putString("taskListKey", json.toString()).apply()
+        sharedPreferences.edit().putString("toDoListKey", json.toString()).apply()
     }
 
     fun getToDoList(): MutableList<TaskItem> {
         val taskList = mutableListOf<TaskItem>()
-        val json = sharedPreferences.getString("taskListKey", null)
+        val json = sharedPreferences.getString("toDoListKey", null)
         json?.split("|")?.forEach { item ->
             item.split(",")?.let { values ->
                 if (values.size == 3) {
