00375a2c2fca374a6b94f777a14a8052e3cec82c
yuto ichinose <yuto.0526.sano@gmail.com>
2023-08-08 11:33:35 +0900

    #1 Gson実装完了

--- app/src/main/java/com/example/kotlinjetpacktodoapp/ToDoPreferences.kt
+++ app/src/main/java/com/example/kotlinjetpacktodoapp/ToDoPreferences.kt
@@ -1,37 +1,33 @@
 package com.example.kotlinjetpacktodoapp
+
 import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 public class ToDoPreferences(context: Context) {
     private val sharedPreferences = context.getSharedPreferences("com.example.kotlinJetpackTodoApp.ToDoPreferences", Context.MODE_PRIVATE)
+    private val gson = Gson()
 
     fun saveToDoCount(count: Int) {
         sharedPreferences.edit().putInt("toDoCount", count).apply()
     }
+
     fun loadToDoCount(): Int {
         return sharedPreferences.getInt("toDoCount", 0)
     }
 
     fun saveToDoList(taskList: List<TaskItem>) {
-        val json = StringBuilder()
-        for (taskItem in taskList) {
-            json.append("${taskItem.id},${taskItem.taskName},${taskItem.isCompleted}|")
-        }
-        sharedPreferences.edit().putString("toDoListKey", json.toString()).apply()
+        val json = gson.toJson(taskList)
+        sharedPreferences.edit().putString("toDoListKey", json).apply()
     }
 
     fun loadToDoList(): List<TaskItem> {
-        val taskList = mutableListOf<TaskItem>()
         val json = sharedPreferences.getString("toDoListKey", null)
-        json?.split("|")?.forEach { item ->
-            item.split(",")?.let { values ->
-                if (values.size == 3) {
-                    val taskId = values[0]
-                    val taskName = values[1]
-                    val isCompleted = values[2].toBoolean()
-                    taskList.add(TaskItem(taskId.toInt(), taskName, isCompleted))
-                }
-            }
+        return if (json != null) {
+            val type = object : TypeToken<List<TaskItem>>() {}.type
+            gson.fromJson(json, type)
+        } else {
+            emptyList()
         }
-        return taskList
     }
 }
